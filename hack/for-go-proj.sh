d4523e023a7ba0639ec0162b81f650232ac98d06
aaronfern <aaron.francis.fernandes@sap.com>
2024-12-04 00:40:40 +0530

    Don't run integration tests along with the test-and-verify script

--- hack/for-go-proj.sh
+++ hack/for-go-proj.sh
@@ -73,6 +73,6 @@ if [ "${CMD}" = "build" ] || [ "${CMD}" == "test" ]; then
   go test -mod vendor -run=None ./...
   popd
   pushd ${CONTRIB_ROOT}/cluster-autoscaler/
-  go test ./...
+  go test $(go list ./... | grep -v integration)
   popd
 fi
